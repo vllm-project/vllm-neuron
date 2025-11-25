1b61b0a00ac0aaae2e3ea0174f426e162056e4e8
Navyadhara Gogineni <navyadha@amazon.com>
2025-11-24 23:57:37 -0800

    Comment out constants and import in constants.py
    Commented out the constants and import statements for future reference.
    
    Signed-off-by: Navyadhara Gogineni <navyadha@amazon.com>

--- vllm_neuron/worker/constants.py
+++ vllm_neuron/worker/constants.py
@@ -1,19 +1,19 @@
-# SPDX-License-Identifier: Apache-2.0
-import torch
+# # SPDX-License-Identifier: Apache-2.0
+# import torch
 
-NEURON_MULTI_MODAL_MODELS = [
-    "MllamaForConditionalGeneration", "LlavaForConditionalGeneration",
-    "Llama4ForConditionalGeneration"
-]
+# NEURON_MULTI_MODAL_MODELS = [
+#     "MllamaForConditionalGeneration", "LlavaForConditionalGeneration",
+#     "Llama4ForConditionalGeneration"
+# ]
 
-TORCH_DTYPE_TO_NEURON_AMP = {
-    "auto": "float32",
-    "half": "float16",
-    "float16": "float16",
-    "bfloat16": "bfloat16",
-    "float": "float32",
-    "float32": "float32",
-    torch.float16: "float16",
-    torch.bfloat16: "bfloat16",
-    torch.float32: "float32",
-}
+# TORCH_DTYPE_TO_NEURON_AMP = {
+#     "auto": "float32",
+#     "half": "float16",
+#     "float16": "float16",
+#     "bfloat16": "bfloat16",
+#     "float": "float32",
+#     "float32": "float32",
+#     torch.float16: "float16",
+#     torch.bfloat16: "bfloat16",
+#     torch.float32: "float32",
+# }
